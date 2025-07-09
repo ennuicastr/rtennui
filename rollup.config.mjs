5f9a059cf533934c6fc5a203da1b326692e4a0a7
Yahweasel <git-rpo@yahweasel.com>
2025-07-09 20:42:03 +0000

    Modernize build

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,22 +3,35 @@ import nodeResolve from "@rollup/plugin-node-resolve";
 import terser from "@rollup/plugin-terser";
 import typescript from "@rollup/plugin-typescript";
 
+const terserPlugin = [terser({
+    format: {
+        preamble: await fs.readFile("src/license.js", "utf8")
+    }
+})];
+
 export default {
     input: "src/main.ts",
     output: [
         {
+            file: "dist/rtennui.mjs",
+            format: "es",
+            plugins: terserPlugin
+        },
+        {
+            file: "dist/rtennui.min.mjs",
+            format: "es",
+            plugins: terserPlugin
+        },
+        {
             file: "dist/rtennui.js",
             format: "umd",
             name: "RTEnnui"
-        }, {
+        },
+        {
             file: "dist/rtennui.min.js",
             format: "umd",
             name: "RTEnnui",
-            plugins: [terser({
-                format: {
-                    preamble: await fs.readFile("src/license.js", "utf8")
-                }
-            })]
+            plugins: terserPlugin
         }
     ],
     plugins: [
